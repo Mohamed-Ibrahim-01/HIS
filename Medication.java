c98c9d3f2d9a13307a525a474822d51ea6d77ed7
mahmoud1000000 <mahmoudsaleem522@gmail.com>
2020-09-26 15:39:00 +0200

    uuid

--- Medication.java
+++ Medication.java
@@ -1,22 +1,31 @@
+import java.util.UUID;
 class Medication{
+    private UUID uuid;
     private String Name;
     private int DailyDose, WeeklyDose, DoseValue, Quantity;
-    public Medication(String Name,int DailyDose, int WeeklyDose, int DoseValue, int Quantity){
+    public Medication(String Name,int DailyDose, int DoseValue, int Quantity,UUID uuid){
         this.Name = Name;
         this.DailyDose = DailyDose;
-        this.WeeklyDose = WeeklyDose;
+        WeeklyDose = (DailyDose*7) ;
         this.DoseValue = DoseValue;
         this.Quantity = Quantity;
+        this.uuid = uuid ;
     }    
     public String getName(){ return Name;}
     public int getDailyDose(){ return DailyDose;}
     public int getWeeklyDose(){ return WeeklyDose;}
     public int getDoseValue(){ return DoseValue;}
     public int getQuantity(){ return Quantity;}
+    public UUID getUuid() {
+        return uuid;
+    }
     
     public void setName(String Name){this.Name = Name;}
     public void setDailyDose(int DailyDose){this.DailyDose = DailyDose;}
     public void setWeeklyDose(int WeeklyDose){this.WeeklyDose = WeeklyDose;}
     public void setDoseValue(int DoseValue){this.DoseValue = DoseValue;}
     public void setQuantity(int Quantity){this.Quantity = Quantity;}
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
 }
