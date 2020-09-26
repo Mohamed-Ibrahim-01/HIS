d283e445e0a860d8006d0522b919fbafe6662b12
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2020-09-26 13:39:03 +0200

    Medication to String in the Medications Map

--- PersonMH.java
+++ PersonMH.java
@@ -5,13 +5,13 @@ class PersonMH{
     private HashMap<String,Date> ChronicDiseases;
     private HashMap<String,Date> Diseases;
     private HashMap<String,Date> Hospitalizations;
-    private HashMap<Medication,Date> Medications;
+    private HashMap<String,Date> Medications;
     private ArrayList<String> GeneticDiseases;
     
     public PersonMH(){
     }
     public PersonMH(HashMap<String,Date> ChronicDiseases,HashMap<String,Date> Diseases
-        ,HashMap<String,Date> Hospitalizations ,HashMap<Medication,Date> Medications,ArrayList<String> GeneticDiseases){
+        ,HashMap<String,Date> Hospitalizations ,HashMap<String,Date> Medications,ArrayList<String> GeneticDiseases){
 
         this.ChronicDiseases = ChronicDiseases;
         this.Hospitalizations = Hospitalizations;
@@ -23,13 +23,13 @@ class PersonMH{
    public HashMap<String,Date> getChronicDiseases(){return this.ChronicDiseases;} 
    public HashMap<String,Date> getDiseases(){return this.Diseases;} 
    public HashMap<String,Date> getHospitalizations(){return this.Hospitalizations;} 
-   public HashMap<Medication,Date> getMedications(){ return this.Medications;}
+   public HashMap<String,Date> getMedications(){ return this.Medications;}
    public ArrayList<String> getGeneticDiseases(){return this.GeneticDiseases;}
    
    public void setChronicDiseases(HashMap<String,Date> ChronicDiseases){this.ChronicDiseases = ChronicDiseases;} 
    public void setDiseases(HashMap<String,Date> Diseases){this.Diseases = Diseases;} 
    public void setHospitalizations(HashMap<String,Date> Hospitalizations){this.Hospitalizations = Hospitalizations;} 
-   public void setMedications(HashMap<Medication,Date> Medications){this.Medications = Medications;}
+   public void setMedications(HashMap<String,Date> Medications){this.Medications = Medications;}
    public void setGeneticDiseases(ArrayList<String> GeneticDiseases){ this.GeneticDiseases = GeneticDiseases;}
    
    public boolean addDisease(String name, Date date){
