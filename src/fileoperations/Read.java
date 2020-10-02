c31a12d9f82061a543cf302365ff1ed91aff5f2f
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2020-10-02 17:04:12 +0200

    Read Prototype

--- src/fileoperations/Read.java
+++ src/fileoperations/Read.java
@@ -1,5 +1,23 @@
 package src.fileoperations;
+import src.models.*;
+import java.util.List;
+import src.system.ICU;
 
 public class Read {
+    public static Patient createPatient(){
+       return null;
+    }
+    public static Doctor createDoctor(){
+       return null;
+    }
     
+    public static List<Patient> readPatients(){
+       return null;
+    }
+    public static List<Doctor> readDoctors(){
+       return null;
+    }
+    public static List<ICU> readIcus(){
+       return null;
+    }
 }
