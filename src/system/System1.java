833577815570b38c1e866874ca80fb7f9fbeb0ed
mahmoud1000000 <mahmoudsaleem522@gmail.com>
2020-10-07 15:21:21 +0200

    implement  to string in  icu.java

--- src/system/System1.java
+++ src/system/System1.java
@@ -4,9 +4,15 @@ import src.fileoperations.*;
 import java.util.List;
 public class System1 {
     public static void main(String[] args){
-        List<Patient> patients = Read.readPatients();
-        for(Patient d : patients){
+       try{
+        List<ICU> icus = Read.readICUs();
+        for(ICU d : icus){
             System.out.println(d);
         }
+    }
+    catch(Exception e){
+
+        System.out.println("Exception occured");
+    }
     }
 }
