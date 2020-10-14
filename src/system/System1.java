a5d610a555e256fc662ce1543c958d456356921c
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2020-10-14 23:46:39 +0200

    Test ICU reading

--- src/system/System1.java
+++ src/system/System1.java
@@ -18,6 +18,8 @@ public class System1 {
     }
     public static void main(String[] args){
         loadAllData();
-        Write.addNewPatient();
+        //Write.addNewPatient();
+        System.out.println( ICUManage.getICU("A1"));
+        //System.out.println(ICUManage.getEmptyBed()[1]);
     }
 }
