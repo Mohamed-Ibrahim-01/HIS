0643ac75a44066c4e67bd937506340fa306ff525
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2020-10-15 17:05:24 +0200

    Test add patient again

--- src/system/System1.java
+++ src/system/System1.java
@@ -18,8 +18,8 @@ public class System1 {
     }
     public static void main(String[] args){
         loadAllData();
-        //Write.addNewPatient();
-        System.out.println( ICUManage.getICU("A1"));
+        Write.addNewPatient();
+        //System.out.println( ICUManage.getICU("A1"));
         //System.out.println(ICUManage.getEmptyBed()[1]);
     }
 }
