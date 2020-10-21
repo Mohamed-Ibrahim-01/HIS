3fd7bd15ad016d5cb3bfe3d6fcb36205511fdac5
Mohamed-Ibrahim-01 <mohamed.ibrahim02023@gmail.com>
2020-10-21 15:58:11 +0200

    implement print all patient, print patient , peint icu , print medical history

--- src/output/CmdOutput.java
+++ src/output/CmdOutput.java
@@ -1,13 +1,60 @@
 package src.output;
 
 import src.models.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Date;
+import java.util.Iterator;
 import src.management.*;
 
 public class CmdOutput {
     public static void printPatient(String name){
         Patient patient = NetworkManage.getPatient(name);
         Prompt.showTitle("  PRINTING PATIENT'S INFORMATION  ");
-        printPerson(patient);
+        printData(patient.getPatientData());
+    }
+    public static void printAllPatients(){
+        Prompt.showTitle("  PRINTING ALL PATIENT'S INFORMATION  ");
+        for(Patient patient : NetworkManage.getPatients())
+            printData(patient.getPatientData());
+    }
+
+    public static void printMedicalHistory(String name){
+        Patient patient = NetworkManage.getPatient(name);
+        Prompt.showTitle("  PRINTING PATIENT'S MEDICAL HISTORY");
+        String[] mapNames = {"Hospitalizations","ChronicDiseases","Diseases","Medications"};
+        for(HashMap<String,Date> map : patient.getMedicalHistory().getPatientMH().getHistory()){
+            int i = 0;
+            System.out.printf("%60s",mapNames[i]);
+            printData(mapToArr(map)); 
+        }
+    }
+    public static void printIcu(String name){
+        ICU icu = ICUManage.getICU(name);
+        Prompt.showTitle("PRINTING ICU DATA");
+        System.out.println(icu);
+    }
+    public static void printAllIcus(){
+        Prompt.showTitle("PRINTING ALL ICUS");
+        for(ICU icu : ICUManage.getIcus())
+            System.out.println(icu);
+    }
+    private static <T,V> String[][] mapToArr(HashMap<T,V> map){
+        String[][] arr = new String[map.size()][2];
+        Set<Map.Entry<T,V>> entries = map.entrySet();
+        Iterator<Map.Entry<T,V>> entriesIterator = entries.iterator();
+        for(int i = 0; entriesIterator.hasNext();i++){
+            Map.Entry<T,V> mapping = (Map.Entry<T,V>) entriesIterator.next();
+            arr[i][0] = mapping.getKey().toString();
+            arr[i][1] = mapping.getValue().toString();
+        }
+        return arr;
+    }
+    private static void printData (String[][] toPrint){
+        for (int i = 0; i < toPrint.length; i++) {
+            System.out.printf("%-40s %40s %n ",toPrint[i][0],toPrint[i][1]);
+        }
     }
 
     private static void printPerson(Person person) {
