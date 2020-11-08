8df1fd4c1d014410435a11f58fcd24a8f58b4a1f
Mohamed-Ibrahim-01 <60495252+Mohamed-Ibrahim-01@users.noreply.github.com>
2020-11-08 02:40:20 +0200

    Fix import

--- src/output/CmdOutput.java
+++ src/output/CmdOutput.java
@@ -80,19 +80,6 @@ public class CmdOutput {
         }
     }
 
-    private static void printPerson(Person person) {
-        Prompt.printDashline();
-        System.out.printf("%-15s %15s %n", "Attributes", "values");
-        Prompt.printDashline();
-        System.out.println();
-        for (int i = 0; i < person.getAttributes().length; i++) {
-            System.out.printf("%-15s %15s %n ", person.getAttributesNames()[i], person.getAttributes()[i]);
-        }
-    }
-
-    // private static String putNewLine(int i , String[] array){
-    // return (i == array.length-1) ? "" : "%n" ;
-    // }
     public static void printDoctor(String name) {
         Doctor doctor = (Doctor) NetworkManage.getDoctor(name);
         if (doctor != null) {
